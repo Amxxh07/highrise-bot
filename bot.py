8c3735ec2b1eec466c493981dee03a3ad99ea231
Amxxh07 <amoghhoon07@gmail.com>
2026-03-14 18:23:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-from highrise import BaseBot
+from highrise import BaseBot, Highrise
 import os
 
 class Bot(BaseBot):
 
     async def on_start(self, session_metadata):
-        print("Bot connected to Highrise!")
+        print("Bot connected!")
 
 bot = Bot()
 
 if __name__ == "__main__":
-    bot.run(
-        room_id=os.environ["65d9f3af863dc01805999f2c"],
-        token=os.environ["23db108357bc3d0ebe4bea1264d2636b37d1c969a77aabfe72c42a768a17fc02"]
-    )
+    room_id = os.getenv("65d9f3af863dc01805999f2c")
+    token = os.getenv("23db108357bc3d0ebe4bea1264d2636b37d1c969a77aabfe72c42a768a17fc02")
+
+    highrise = Highrise(bot)
+    highrise.run(room_id, token)
