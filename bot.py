2c0e161d0a8b67712b472936dcd6f7800234ed03
Amxxh07 <amoghhoon07@gmail.com>
2026-03-14 18:14:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 from highrise import BaseBot, Highrise
 from highrise.models import *
 
@@ -7,3 +8,11 @@ class Bot(BaseBot):
         print("Bot is online!")
 
 bot = Bot()
+
+app = Highrise(
+    bot,
+    "<65d9f3af863dc01805999f2c>",
+    "<23db108357bc3d0ebe4bea1264d2636b37d1c969a77aabfe72c42a768a17fc02>"
+)
+
+app.run()
